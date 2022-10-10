58f1da93056d090c151a73b8786401c262ae3f6b
Dima Sorokin <sorokindima36@gmail.com>
2022-10-10 17:58:34 +0300

    1.1

--- WebApplication2/Entities/BaseModel.Context.cs
+++ WebApplication2/Entities/BaseModel.Context.cs
@@ -13,10 +13,10 @@ namespace WebApplication2.Entities
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Sfera_IT_PPEntities1 : DbContext
+    public partial class Sfera_IT_PPEntities3 : DbContext
     {
-        public Sfera_IT_PPEntities1()
-            : base("name=Sfera_IT_PPEntities1")
+        public Sfera_IT_PPEntities3()
+            : base("name=Sfera_IT_PPEntities3")
         {
         }
     
